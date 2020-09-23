70101bd84408bde28d402159219c12a853497c81
jomolinam <70985740+jomolinam@users.noreply.github.com>
2020-09-23 18:58:58 -0500

    Update sum4b.v

--- quartus/sum4b/sum4b.v
+++ quartus/sum4b/sum4b.v
@@ -1,3 +1,5 @@
+`timescale 1ns / 1ps
+
 module sum4b (A,B,Cout,S);
 
   input [3:0] A;
@@ -7,10 +9,10 @@ module sum4b (A,B,Cout,S);
   
   wire c1,c2,c3;
   
-  sum1b (.A(A[0]),.B(B[0]),.Ci(0),.Cout(c1),.S(S[0]));
-  sum1b (.A(A[1]),.B(B[1]),.Ci(c1),.Cout(c2),.S(S[1]));
-  sum1b (.A(A[2]),.B(B[2]),.Ci(c2),.Cout(c3),.S(S[2]));
-  sum1b (.A(A[3]),.B(B[3]),.Ci(c3),.Cout(cout),.S(S[3]));
+  sum1b s0(.A(A[0]),.B(B[0]),.Ci(0),.Cout(c1),.S(S[0]));
+  sum1b s1(.A(A[1]),.B(B[1]),.Ci(c1),.Cout(c2),.S(S[1]));
+  sum1b s2(.A(A[2]),.B(B[2]),.Ci(c2),.Cout(c3),.S(S[2]));
+  sum1b s3(.A(A[3]),.B(B[3]),.Ci(c3),.Cout(cout),.S(S[3]));
   
   
   endmodule
