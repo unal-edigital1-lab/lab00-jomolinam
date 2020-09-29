6ef5b705e42bd890131ccd398d378d978be96642
jomolinam <70985740+jomolinam@users.noreply.github.com>
2020-09-29 14:53:10 -0500

    Update sum4b_TB.v

--- quartus/sum4b/sum4b_TB.v
+++ quartus/sum4b/sum4b_TB.v
@@ -11,16 +11,24 @@ module sum4b_TB;
  
   
   //instanciar
-  sum4b cn ( .A(A), .B(B), .Cout(Co), .S(S) );
+  sum4b uut ( .A(A), .B(B), .Cout(Co), .S(S) );
   
   initial begin
+  
   A=0;
   B=0;
   #4 A=2;
   #4 B=5; A=3;
   #4 B=15;
   #7 $stop;
-  
+ /*
+  A=0;
+  for(B = 0; B < 16 ; B = B + 1) begin
+    if(B==0)
+    A = A + 1;
+      #2;  
+  end
+   */
   end
 
 endmodule
